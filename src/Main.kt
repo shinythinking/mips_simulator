1ce23fb6dbdedadfa3436b79dade3cab6a2311a4
shinythinking <shinythink@gmail.com>
2026-03-30 15:01:05 +0900

    feat: load method for bin

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import core.ArchitecturalState
+import simulator.Simulator
+import java.io.File
+import java.io.FileNotFoundException
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("오류: 실행 시 파일 이름을 인수로 전달해주세요.")
+        return
+    }
+
+    val filename = args[0]
+
+    val codes = readFile(filename)
+
+    val architecturalState = ArchitecturalState(codes)
+
+    val simulator = Simulator()
+    simulator(architecturalState)
+}
+
+fun readFile(filename: String): ByteArray {
+    try {
+        val file = File(filename)
+        val bytes = file.readBytes()
+
+        return bytes
+
+    } catch (fileNotFound: FileNotFoundException) {
+        throw FileNotFoundException("File not found: $filename: ${fileNotFound.message}")
     }
 }
